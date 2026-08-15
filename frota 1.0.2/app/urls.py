55270f8318f8fb015eda34ec1a205d55f489d299
miguelcamerini <miguelcamerini@gmail.com>
2026-08-15 17:29:48 -0300

    Delete frota 1.0.2 directory

--- frota 1.0.2/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('login_view/', views.login_view, name='login_view'),
-    path('home/', views.home, name='home'),
-
-]
